20f29ce58b3eb73f1744f1b20547a05bab58fb4f
xNightish <pandaubiica@gmail.com>
2024-11-16 13:56:06 +0300

    created

--- knnreg.py
+++ knnreg.py
@@ -5,7 +5,14 @@ import pandas as pd
 class MyKNNReg:
     def __init__(self, k = 3):
         self.k = k
-        
+        self.train_size = 0
         
     def __repr__(self):
         return f'MyKNNReg class: k={self.k}'
+    
+    
+    def fit(self, X, y):
+        self.X_train = X.copy()
+        self.y_train = y.copy()
+        self.train_size = X.shape
+        return self.train_size
