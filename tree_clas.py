ff506bc2debc331a989e974205a6940820f9c0fb
xNightish <pandaubiica@gmail.com>
2024-11-21 17:49:54 +0300

    add fit and visualize_tree

--- tree_clas.py
+++ tree_clas.py
@@ -1,70 +1,117 @@
 import numpy as np
 import pandas as pd
-from sklearn.datasets import make_classification
+from graphviz import Digraph
 
-X, y = make_classification(n_samples=1000, n_features=14, n_informative=10, random_state=42)
-X = pd.DataFrame(X)
-y = pd.Series(y)
-X.columns = [f'col_{col}' for col in X.columns]
-
+# Загружаем данные
+df = pd.read_csv('https://archive.ics.uci.edu/static/public/267/banknote+authentication.zip', header=None)
+df.columns = ['variance', 'skewness', 'curtosis', 'entropy', 'target']
+X, y = df.iloc[:, :4], df['target']
 
 class MyTreeClf:
     
-    def __init__(self, max_depth=5, min_samples_split=2, max_leafs=20):
+    def __init__(self, max_depth=15, min_samples_split=2, max_leafs=20):
         self.max_depth = max_depth
         self.min_samples_split = min_samples_split
         self.max_leafs = max_leafs
-        
-    def __repr__(self):
-        return f'MyTreeClf class: max_depth={self.max_depth}, min_samples_split={self.min_samples_split}, max_leafs={self.max_leafs}'
-    
+        self.leafs_cnt = 0  # Счетчик листьев
+        self.tree = None  # Дерево решений
+
     def entropy(self, y):
-        """Calculate entropy of a label array."""
         value_counts = y.value_counts(normalize=True)
-        return -np.sum(value_counts * np.log2(value_counts + 1e-12))  # Добавляем малое значение для избежания логарифма от 0
+        return -np.sum(value_counts * np.log2(value_counts + 1e-12))
 
     def get_best_split(self, X, y):
         best_ig = -np.inf
         best_col_name = None
         best_split_value = None
+        parent_entropy = self.entropy(y)
 
         for col_name in X.columns:
             unique_values = X[col_name].unique()
-            unique_values.sort()  # Сортируем уникальные значения для корректного формирования сплитов
+            unique_values.sort()
 
             for i in range(len(unique_values) - 1):
-                split_value = (unique_values[i] + unique_values[i + 1]) / 2  # Находим среднее значение между двумя уникальными значениями
+                split_value = (unique_values[i] + unique_values[i + 1]) / 2
 
-                # Создаем подвыборки
                 left_mask = X[col_name] <= split_value
                 right_mask = X[col_name] > split_value
 
                 left_y = y[left_mask]
                 right_y = y[right_mask]
 
-                # Проверяем, достаточно ли примеров в подвыборках
                 if len(left_y) == 0 or len(right_y) == 0:
                     continue
 
-                # Вычисляем прирост информации
-                parent_entropy = self.entropy(y)
                 left_entropy = self.entropy(left_y)
                 right_entropy = self.entropy(right_y)
 
-                # Прирост информации
                 ig = parent_entropy - (len(left_y) / len(y) * left_entropy + len(right_y) / len(y) * right_entropy)
 
-                # Проверяем, является ли текущий сплит лучшим
                 if ig > best_ig:
                     best_ig = ig
                     best_col_name = col_name
                     best_split_value = split_value
 
         return best_col_name, best_split_value, best_ig
+    
+    def fit(self, X, y, depth=0, node=None):
+        if node is None:
+            self.tree = {}
+            node = self.tree
+            is_root = True
+        else:
+            is_root = False
 
+        if (depth >= self.max_depth or len(y) < self.min_samples_split or len(set(y)) == 1 or self.max_leafs - self.leafs_cnt <= 1) and is_root == False:
+            node['leaf'] = y.value_counts(normalize=True).to_dict()
+            self.leafs_cnt += 1
+            return
 
+        col_name, split_value, ig = self.get_best_split(X, y)
+
+        node[col_name] = {}
+        node[col_name][f'<= {split_value}'] = {}
+        node[col_name][f'> {split_value}'] = {}
+
+        left_mask = X[col_name] <= split_value
+        right_mask = X[col_name] > split_value
+
+        self.fit(X[left_mask], y[left_mask], depth + 1, node[col_name][f'<= {split_value}'])
+        self.fit(X[right_mask], y[right_mask], depth + 1, node[col_name][f'> {split_value}'])
+
+    def visualize_tree(self, node=None, graph=None, parent=None, edge_label=''):
+        if graph is None:
+            graph = Digraph()
+        
+        if node is None:
+            node = self.tree
+
+        for key, value in node.items():
+            if key == 'leaf':
+                leaf_str = ', '.join([f'{k}: {v:.2f}' for k, v in value.items()])
+                graph.node(str(id(node)), f'Leaf: {leaf_str}', shape='box', color='lightblue')
+                if parent is not None:
+                    graph.edge(parent, str(id(node)), label=edge_label)
+            else:
+                split_value = list(value.keys())[0].split()[1]
+                graph.node(str(id(value)), f'{key} <= {split_value}')
+                if parent is not None:
+                    graph.edge(parent, str(id(value)), label=edge_label)
+
+                self.visualize_tree(value[f'<= {split_value}'], graph, str(id(value)), '<=' + split_value)
+                self.visualize_tree(value[f'> {split_value}'], graph, str(id(value)), '>' + split_value)
+
+        return graph
+    
+print(df.head(5))
 
 # Создаем и тестируем классификатор
-clf = MyTreeClf()
-col_name, split_value, ig = clf.get_best_split(X, y)
-print(f'Best split: Column="{col_name}", Value={split_value}, Information Gain={ig}')
+clf = MyTreeClf(max_depth=3, min_samples_split=2, max_leafs=1)
+clf.fit(X, y)
+
+# Визуализируем дерево
+tree_graph = clf.visualize_tree()
+tree_graph.render('decision_tree', format='png', cleanup=True)  # Сохраняем в формате PNG
+
+print(clf.leafs_cnt)
+
