7d435ff3dc2e525ec21f6eb14112d15f100d233c
xNightish <pandaubiica@gmail.com>
2024-11-24 16:12:14 +0300

    add gini, fixed bugs, update graph

--- tree_clas.py
+++ tree_clas.py
@@ -9,7 +9,7 @@ X, y = df.iloc[:, :4], df['target']
 
 class MyTreeClf:
     
-    def __init__(self, max_depth=15, min_samples_split=2, max_leafs=20, bins=None):
+    def __init__(self, max_depth=15, min_samples_split=2, max_leafs=20, bins=None, criterion='entropy'):
         self.max_depth = max_depth
         self.min_samples_split = min_samples_split
         self.max_leafs = max_leafs
@@ -17,24 +17,33 @@ class MyTreeClf:
         self.leafs_cnt = 0 
         self.tree = None
         self.histograms = {}
+        self.criterion = criterion
+        self.reserved_leaves = 0
 
     def entropy(self, y):
         value_counts = y.value_counts(normalize=True)
-        return -np.sum(value_counts * np.log2(value_counts + 1e-12))
+        return -np.sum(value_counts * np.log2(value_counts))
+    
+    def gini(self, y):
+        value_counts = y.value_counts(normalize=True)
+        return 1 - np.sum(value_counts**2)
+    
+    
+    def impurity(self, y):
+        criterion = {'entropy': self.entropy, 'gini': self.gini}
+        return criterion[self.criterion](y)
 
     def get_best_split(self, X, y):
         best_ig = -np.inf
         best_col_name = None
         best_split_value = None
-        parent_entropy = self.entropy(y)
+        parent_impurity = self.impurity(y)
 
         for col_name in X.columns:
             unique_values = X[col_name].unique()
             unique_values.sort()
 
-            # Определяем разделители
             if self.bins is not None:
-                # Используем гистограмму для определения разделителей
                 if col_name not in self.histograms:
                     hist, bin_edges = np.histogram(X[col_name], bins=self.bins)
                     self.histograms[col_name] = bin_edges[1:-1]  # Убираем крайние значения
@@ -56,10 +65,10 @@ class MyTreeClf:
                 if len(left_y) == 0 or len(right_y) == 0:
                     continue
 
-                left_entropy = self.entropy(left_y)
-                right_entropy = self.entropy(right_y)
+                left_impurity = self.impurity(left_y)
+                right_impurity = self.impurity(right_y)
 
-                ig = parent_entropy - (len(left_y) / len(y) * left_entropy + len(right_y) / len(y) * right_entropy)
+                ig = parent_impurity - (len(left_y) / len(y) * left_impurity + len(right_y) / len(y) * right_impurity)
 
                 if ig > best_ig:
                     best_ig = ig
@@ -76,14 +85,15 @@ class MyTreeClf:
         else:
             is_root = False
 
-        if (depth >= self.max_depth or len(y) < self.min_samples_split or len(set(y)) == 1 or self.max_leafs - self.leafs_cnt <= 1) and is_root == False:
+        if (depth >= self.max_depth or len(y) < self.min_samples_split or len(set(y)) == 1 or 
+            self.max_leafs + 1 - self.leafs_cnt - self.reserved_leaves <= 0) and not is_root:
             node['leaf'] = y.value_counts(normalize=True).to_dict()
             self.leafs_cnt += 1
             return
 
         col_name, split_value, ig = self.get_best_split(X, y)
 
-        if col_name is None:  # Если нет подходящего разделителя
+        if col_name is None:
             node['leaf'] = y.value_counts(normalize=True).to_dict()
             self.leafs_cnt += 1
             return
@@ -92,47 +102,74 @@ class MyTreeClf:
         node[col_name][f'<= {split_value}'] = {}
         node[col_name][f'> {split_value}'] = {}
 
+        self.reserved_leaves += 1
+
         left_mask = X[col_name] <= split_value
         right_mask = X[col_name] > split_value
 
         self.fit(X[left_mask], y[left_mask], depth + 1, node[col_name][f'<= {split_value}'])
         self.fit(X[right_mask], y[right_mask], depth + 1, node[col_name][f'> {split_value}'])
+        
+        self.reserved_leaves -= 1
+
 
     def visualize_tree(self, node=None, graph=None, parent=None, edge_label=''):
         if graph is None:
             graph = Digraph()
-        
+
         if node is None:
             node = self.tree
 
         for key, value in node.items():
             if key == 'leaf':
                 leaf_str = ', '.join([f'{k}: {v:.2f}' for k, v in value.items()])
-                graph.node(str(id(node)), f'Leaf: {leaf_str}', shape='box', color='lightblue')
+                graph.node(str(id(node)), f'Leaf: {leaf_str}', shape='box', style='filled', fillcolor='lightgreen')  # Лист зеленый
                 if parent is not None:
-                    graph.edge(parent, str(id(node)), label=edge_label)
+                    # Цвет стрелки определяется по edge_label
+                    edge_color = 'blue' if '<=' in edge_label else 'red'
+                    graph.edge(parent, str(id(node)), label=edge_label, color=edge_color)  # Стрелка с цветом
             else:
                 split_value = list(value.keys())[0].split()[1]
-                graph.node(str(id(value)), f'{key} <= {split_value}')
-                if parent is not None:
-                    graph.edge(parent, str(id(value)), label=edge_label)
+                left_node = value[f'<= {split_value}']
+                right_node = value[f'> {split_value}']
 
-                self.visualize_tree(value[f'<= {split_value}'], graph, str(id(value)), '<=' + split_value)
-                self.visualize_tree(value[f'> {split_value}'], graph, str(id(value)), '>' + split_value)
+                # Устанавливаем цвет узла в зависимости от вероятностей
+                left_leaf_probability = left_node.get('leaf', {}).get(1, 0)
+                right_leaf_probability = right_node.get('leaf', {}).get(1, 0)
+
+                # Устанавливаем цвет узла (ветви) и стрелки
+                if parent is None:
+                    fill_color = '#FFA500'  # Оранжевый для корня
+                else:
+                    fill_color = '#FFCCCB' if left_leaf_probability > right_leaf_probability else '#ADD8E6'  # Красный или голубой для ветвей
+
+                graph.node(str(id(value)), f'{key} <= {split_value}', style='filled', fillcolor=fill_color)
+
+                if parent is not None:
+                    # Цвет стрелки определяется на основе edge_label
+                    edge_color = 'blue' if '<=' in edge_label else 'red'
+                    graph.edge(parent, str(id(value)), label=edge_label, color=edge_color)
+
+                self.visualize_tree(left_node, graph, str(id(value)), '<=' + split_value)
+                self.visualize_tree(right_node, graph, str(id(value)), '>' + split_value)
 
         return graph
-    
-    
+
     def _class(self, row):
         node = self.tree
-        while 'leaf' not in node:
+        while True:
+            if 'leaf' in node:
+                return node['leaf'].get(1, 0) 
+            if not node or not node.keys():
+                return 0 
             col_name = list(node.keys())[0]
+            if col_name not in node:
+                return 0 
             split_value = list(node[col_name].keys())[0].split()[1]
             if row[col_name] <= float(split_value):
                 node = node[col_name][f'<= {split_value}']
             else:
                 node = node[col_name][f'> {split_value}']
-        return node['leaf'].get(1, 0)
 
     def predict_proba(self, X):
         return X.apply(self._class, axis=1)
@@ -154,7 +191,7 @@ class MyTreeClf:
     
     
 # Создаем и тестируем классификатор
-clf = MyTreeClf(max_depth=4, min_samples_split=100, max_leafs=17, bins=16)
+clf = MyTreeClf(max_depth=10, min_samples_split=40, max_leafs=21, bins=10, criterion='gini')
 clf.fit(X, y)
 
 # Визуализируем дерево
